1cc472fae408fb03b835446690634bce37cbb109
choijihyeon <gus1043@gmail.com>
2023-12-08 18:11:42 +0900

    CCTV 안전으로 크롤링 키워드 변경

--- CarrotMarket/JiHyeon/AutoCrawling/CctvNewsCrawling.py
+++ CarrotMarket/JiHyeon/AutoCrawling/CctvNewsCrawling.py
@@ -10,7 +10,7 @@ contents = []
 timestamps = []
 
 for i in range(1, 1000000, 10):
-    link = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=CCTV&sort=1&photo=0&field=0&pd=2&ds=2023.11.05&de=2023.12.05&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:dd,p:1m,a:all&start=" + str(i)
+    link = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=CCTV%20%EC%95%88%EC%A0%84&sort=1&photo=0&field=0&pd=2&ds=2023.11.08&de=2023.12.08&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:dd,p:1m,a:all&start=" + str(i)
     response = requests.get(link)
     soup = BeautifulSoup(response.text, 'html.parser')
     url = soup.select('#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > a')
