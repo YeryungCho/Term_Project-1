a2d635adea5bd813829aa7e70316d0a3e19bfcaf
choijihyeon <gus1043@gmail.com>
2023-12-08 21:33:16 +0900

    네이버 기사 4000개까지 제공 이후 동일 링크로 코드 수정

--- CarrotMarket/JiHyeon/AutoCrawling/PoliceNewsCrawling.py
+++ CarrotMarket/JiHyeon/AutoCrawling/PoliceNewsCrawling.py
@@ -5,17 +5,21 @@ from newspaper import Article, ArticleException
 import pandas as pd
 from datetime import datetime
 
+# Initialize counters
+total_articles = 0
+max_articles = 4000
+
 titles = []
 contents = []
 timestamps = []
 
-for i in range(1, 1000000,10):
+for i in range(1, 1000000, 10):
     link = 'https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EA%B2%BD%EC%B0%B0%EC%84%9C%20%EC%95%88%EC%A0%84&sort=1&photo=0&field=0&pd=2&ds=2023.11.08&de=2023.12.08&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:dd,p:1m,a:all&start=' + str(i)
     response = requests.get(link)
     soup = BeautifulSoup(response.text, 'html.parser')
     original_url_elements = soup.select('#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > a')
     original_url = original_url_elements[0]['href'] if original_url_elements else None
-    
+
     # Extract news titles and links
     news_titles = soup.find_all('a', class_='news_tit')
 
@@ -23,10 +27,14 @@ for i in range(1, 1000000,10):
     if len(news_titles) < 10:
         print(f" - {i//10 + 1}페이지에서 기사 수가 10개 미만이므로 크롤링 중단")
         break
-    
+
     for title in news_titles:
+        if total_articles >= max_articles:
+            print(f"총 {max_articles}개의 기사 크롤링 완료. 크롤링 중단.")
+            break
+
         news_url = title['href']
-        
+
         if original_url:
             news_url = original_url
 
@@ -41,12 +49,14 @@ for i in range(1, 1000000,10):
             contents.append(article.text)
             timestamps.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
+            # Increment the total_articles counter
+            total_articles += 1
+
             print(f" - {i//10 + 1}페이지 크롤링 완료: {title.get_text(strip=True)}")
 
         except ArticleException as e:
             # Handle the exception based on the status code
             print(f" - {i//10 + 1}페이지: {title.get_text(strip=True)} - ArticleException (skipping)")
-
 
 # Create a DataFrame
 data = {'제목': titles, '기사 내용': contents, '타임스탬프': timestamps}
