714cb624762de503067c27f2d0d95130735b67a0
choijihyeon <gus1043@gmail.com>
2023-12-08 18:19:24 +0900

    크롤링 키워드 변경

--- CarrotMarket/JiHyeon/AutoCrawling/SubwayNewsCrawling.py
+++ CarrotMarket/JiHyeon/AutoCrawling/SubwayNewsCrawling.py
@@ -10,7 +10,7 @@ contents = []
 timestamps = []
 
 for i in range(1, 1000000, 10):
-    link = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EC%A7%80%ED%95%98%EC%B2%A0%EC%97%AD%20%ED%8E%B8%EC%9D%98%20%EC%A0%91%EA%B7%BC%EC%84%B1%20%ED%8E%B8%EB%A6%AC&sort=1&photo=0&field=0&pd=2&ds=2023.11.08&de=2023.12.08&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:dd,p:1m,a:all&start=" + str(i)
+    link = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EC%A7%80%ED%95%98%EC%B2%A0%EC%97%AD%20%EC%A0%91%EA%B7%BC%EC%84%B1&sort=1&photo=0&field=0&pd=2&ds=2023.11.08&de=2023.12.08&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:dd,p:1m,a:all&start=" + str(i)
     response = requests.get(link)
     soup = BeautifulSoup(response.text, 'html.parser')
     url = soup.select('#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > a')
